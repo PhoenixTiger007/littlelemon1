5ee77e8b950843eb966b8b8e20e1aeb7aabfe381
Anthony Foran <anthony@foranmarketing.com>
2024-08-14 14:52:47 +0200

    Update Files

---
+++ little-lemon/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+function Footer() {
+    return (
+      <footer>
+        <p>Copyright © 2024 Little Lemon. All rights reserved.</p>
+      </footer>
+    );
+  }
+  export default Footer;
+  
---
+++ little-lemon/src/Components/Nav.jsx
@@ -0,0 +1,16 @@
+
+
+function Nav() {
+  return (
+    <nav>
+      <img src="./assets/images/Logo.jpg" alt="Little Lemon Logo" />
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
